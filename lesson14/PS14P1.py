cf1e4d0346416fa9288f5e34c2b915ace8bd2930
AlexShred <126178556+AlexShred@users.noreply.github.com>
2026-04-09 12:01:13 -0500

    Add files via upload

---
+++ lesson14/PS14P1.py
@@ -0,0 +1,74 @@
+#CIS 106 Session 13 Assignment - Dynamic Arrays --- Moldoshev Alisher
+
+# TASK 1
+#n = int(input('Enter a number: '))
+#numList = []
+#for i in range(n):
+#    num = int(input('Enter a number: '))
+#    numList.append(num)
+
+#print(numList)
+
+# TASK 2
+#numList.insert(1, 99)
+#print(numList)
+
+# TASK 3
+#numList[1] = 100
+#print(numList)
+
+# TASK 4
+#numListAdd = [500, 600, 700, 800, 900]
+#print(numListAdd)
+#numList.extend(numListAdd)
+#print(numList)
+
+# TASK 5
+#numList.remove(800)
+#print(numList)
+
+# TASK 6
+#del numList[2]
+#print(numList)
+
+# TASK 7
+
+#grades = ["A", "B", "C", "A", "A", "C"]
+#print(grades)
+
+# TASK 8
+#print(grades.count("A"))
+
+# TASK 9
+#print(grades.index("B"))
+
+# TASK 10
+#if "F" in grades:
+#    print(grades.index("F"))
+#else:
+#    print("F is not in the list")
+
+# TASK 11
+#grades.clear()
+#print(grades)
+
+# TASK 12
+#del grades
+#print(grades)
+
+# TASK 13
+players = ["Rizzo", "Davis", "Baez", "Happ", "Bryan"]
+print(players)
+
+# TASK 14
+players.sort()
+print(players)
+
+# TASK 15
+players2 = players.copy()
+print(players2)
+
+# TASK 16
+players2.reverse()
+print(players)
+print(players2)
